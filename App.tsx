b4fdd741f705bbd576a38da9fd37f438294ac83b
Ahmed1912171 <92374336+Ahmed1912171@users.noreply.github.com>
2023-01-04 19:34:50 +0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { TailwindProvider } from "tailwind-rn";
+import utilities from "./tailwind.json";
+import { NavigationContainer } from "@react-navigation/native";
+import RootNavigator from "./navigator/RootNavigator";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+
+
+const client = new ApolloClient({
+  uri: "http://localhost:5001/api/torrid-parrot", //For Android Physical device write device ip instead of localhost
+  cache: new InMemoryCache(),
+});
+
+export default function App() {
+  return (
+    // @ts-ignore - TailwindProvider is missing a type definition
+    <TailwindProvider utilities={utilities}>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
+    </TailwindProvider>
+  );
+}
